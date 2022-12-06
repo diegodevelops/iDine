aef54e6a85f3d057bf4a8ceda6a143e9c893ec1d
Diego Pérez <diegodevelops@gmail.com>
2022-12-06 16:25:11 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Diego A. Perez Pares on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
