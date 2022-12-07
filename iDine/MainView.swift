80ef81ee0f60a78503ef46727b85633354746471
Diego Pérez <diegodevelops@gmail.com>
2022-12-07 15:32:25 -0400

    Add OrderView

---
+++ iDine/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Diego A. Perez Pares on 12/7/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
